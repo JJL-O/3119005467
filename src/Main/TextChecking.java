ec80361fe94ce8fd640a53df210b39ec2e38a1e0
3119005467刘健君 <1622873848@qq.com>
2021-09-15 23:59:03 +0800

    Complete version

--- src/Main/TextChecking.java
+++ src/Main/TextChecking.java
@@ -10,8 +10,22 @@ import java.io.IOException;
 public class TextChecking {
     public static void main(String[] args) throws IOException {
 
+              String textOrigPath=args[0];
+              String textOrig_FakerPath=args[1];
+              String outPutPath=args[2];
 
+              //根据命令行输入的路径读取和输出相应的文本
+              String str0 = TextIOUtil.readTxt(textOrigPath);
+              String str1 = TextIOUtil.readTxt(textOrig_FakerPath);
 
+              //获取相似度
+              String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+
+              //将相似度写入对应的路径中
+              TextIOUtil.writeTxt(similarity,outPutPath);
+
+              //退出程序
+               System.exit(0);
     }
 }
 
--- src/Main/TextCheckingTest.java
+++ src/Main/TextCheckingTest.java
@@ -1,21 +1,142 @@
 package Main;
 
-import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+import Util.HammingUtil;
+import Util.SimhashUtil;
+import Util.TextIOUtil;
 import org.junit.Test;
 
 /**
  * @Description: TextCheckingTest$
- * @Author Tan
+ * @Author LJJ
  * @Date: 2021/9/14$ 22:15$
  * @Version 1.0
  */
 public class TextCheckingTest {
-@Test
-        public void test(){
+
+    @Test
+    public void origAndOrigAddTest(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_add.txt");
+
+       String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+       TextIOUtil.writeTxt("orig与orig_0.8_add的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void origAndOrig_0_8DelTest(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_del.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_del的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void origAndOrigDelTest(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_del.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_0.8_del的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void origAndOrigDis_1Test(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_dis_1.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_0.8_dis_1的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
 
 
+    @Test
+    public void origAndOrigDis_5Test(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_dis_5.txt");
 
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_0.8_dis_5的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void origAndOrigDis_8Test(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_dis_8.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_0.8_dis_8的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void origAndOrigDis_10Test(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_dis_10.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_0.8_dis_10的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+
+    @Test
+    public void origAndOrig_Dis_15Test(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_dis_15.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_0.8_dis_15的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void origAndOrigMixTest(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_mix.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_0.8_mix的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
 
 
     }
+
+    @Test
+    public void origAndOrigSubTest(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_sub.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_sub的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void origAndOrigSub5Test(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_sub5.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与orig_sub5的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void origAndOrigWrongTest(){
+        String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig.txt");
+        String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_wrong.txt");
+
+        String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+        TextIOUtil.writeTxt("orig与rig_wrong的相似度为"+similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+    }
+
+    @Test
+    public void noExistPathTest() {
+        try {
+            String str0 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orignoexistpath.txt");
+            String str1 = TextIOUtil.readTxt("D:\\软件工程\\个人项目作业\\测试文本\\orig_0.8_add.txt");
+
+            String similarity = HammingUtil.getSimilarity(SimhashUtil.getSimhash(str0), SimhashUtil.getSimhash(str1));
+            TextIOUtil.writeTxt("orig与orig_0.8_add的相似度为" + similarity, "D:\\软件工程\\个人项目作业\\测试文本\\similarity.txt");
+        }catch(Exception e){
+        }
+    }
+
+
 }
+
